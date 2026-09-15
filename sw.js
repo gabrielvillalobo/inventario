129bb144a0f1cb0261635b7f95957007b38243a6
gabrielvillalobo <gabrielvictorvillalobo@gmail.com>
2026-09-15 12:36:53 -0300

    Borrar recuentos, contar variantes en la lista y sin Cura Brochero

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Recuento Mina Clavero · service worker */
-var CACHE='recuento-v4';
+var CACHE='recuento-v6';
 var ARCHIVOS=['./','./index.html','./manifest.webmanifest','./productos.json',
   './icon-180.png','./icon-192.png','./icon-512.png','./icon-512-maskable.png',
   'https://cdn.jsdelivr.net/npm/@zxing/library@0.21.3/umd/index.min.js'];
